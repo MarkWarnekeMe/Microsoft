f9c9eb44385acbc976d983db84de8ff7af722eab
Mark Warneke <mark.warneke@microsoft.com>
2020-12-22 09:58:38 +0100

    try this

--- terraform/Microsoft/provider.tf
+++ terraform/Microsoft/provider.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
-    azure = {
-      source  = "hasicorp/azurerm"
-      version = "~> 2.41.0"
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.41.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  # Configuration options
 }
