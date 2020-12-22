b22cb54c988e552076569c58f4191436ce7b5378
Mark Warneke <mark.warneke@microsoft.com>
2020-12-22 14:23:08 +0100

    try network module

--- terraform/Microsoft/main.tf
+++ terraform/Microsoft/main.tf
@@ -1,5 +1,13 @@
 
-resource "azurerm_resource_group" "rg-hello-azure" {
-  name     = "rg-hello-azure"
-  location = "northcentralus"
+locals {
+  name     = "markwarnekeme"
+  location = "westeurope"
+}
+
+
+module "network" {
+  source              = "./network"
+  name                = local.name
+  location            = local.location
+  resource_group_name = local.name + "-vnet"
 }
---
+++ terraform/Microsoft/network/main.tf
@@ -0,0 +1,92 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "random_id" "randomId" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.main
+  }
+
+  byte_length = 10
+}
+
+locals {
+  fixed_tags = {
+    id        = random_id.randomId.hex
+    terraform = "true"
+  }
+
+  tags = merge(var.tags, local.fixed_tags)
+}
+
+
+resource "azurerm_virtual_network" "main" {
+  name                = var.name + "-net"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["192.168.0.0/16"]
+
+  tags = local.tags
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = var.name + "-backend"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["192.168.0.0/24"]
+}
+
+resource "azurerm_network_security_group" "deny" {
+  name                = var.name + "-deny-nsg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "restrict"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "backend-deny" {
+  subnet_id                 = azurerm_subnet.backend.id
+  network_security_group_id = azurerm_network_security_group.deny.id
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = var.name + "-frontend"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["192.168.1.0/25"]
+}
+
+resource "azurerm_network_security_group" "allow80" {
+  name                = var.name + "-allow-nsg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "allow"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "allow"
+    protocol                   = "Tcp"
+    source_port_range          = "80"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "frontend-allow80" {
+  subnet_id                 = azurerm_subnet.frontend.id
+  network_security_group_id = azurerm_network_security_group.allow80.id
+}
