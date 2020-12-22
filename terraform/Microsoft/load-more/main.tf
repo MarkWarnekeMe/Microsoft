21c13bc59f5bd07ffd1e80aea00b909f99ec7336
Mark Warneke <mark.warneke@microsoft.com>
2020-12-22 17:39:03 +0100

    comment out kubernetes for now

--- terraform/Microsoft/load-more/main.tf
+++ terraform/Microsoft/load-more/main.tf
@@ -1,11 +1,34 @@
-module "kubernetes" {
-  source                     = "../Modules/kubernetes"
-  name                       = var.name
-  location                   = var.location
-  resource_group_name        = azurerm_resource_group.main.name
-  log_analytics_workspace_id = module.log_analytics_workspace.id
-  tags                       = local.tags
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
 }
+
+resource "random_id" "randomId" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.main.name
+  }
+
+  byte_length = 10
+}
+
+locals {
+  fixed_tags = {
+    id        = random_id.randomId.hex
+    terraform = "true"
+  }
+
+  tags = merge(var.tags, local.fixed_tags)
+}
+
+# module "kubernetes" {
+#   source                     = "../Modules/kubernetes"
+#   name                       = var.name
+#   location                   = var.location
+#   resource_group_name        = azurerm_resource_group.main.name
+#   log_analytics_workspace_id = module.log_analytics_workspace.id
+#   tags                       = local.tags
+# }
 
 module "cosmosdb_account" {
   source                     = "../Modules/cosmosdb_account"
