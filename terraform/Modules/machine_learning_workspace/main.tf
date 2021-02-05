79d1a2548ee0c8ea21a003e209d370cd208ed348
Mark Warneke <mark.warneke@microsoft.com>
2021-02-05 17:02:28 +0100

    add diagnostics

--- terraform/Modules/machine_learning_workspace/main.tf
+++ terraform/Modules/machine_learning_workspace/main.tf
@@ -44,8 +44,7 @@ resource "azurerm_storage_account" "main" {
 }
 
 resource "azurerm_machine_learning_workspace" "main" {
-  name = var.name
-
+  name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
 
@@ -61,4 +60,47 @@ resource "azurerm_machine_learning_workspace" "main" {
   }
 
   tags = var.tags
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic" {
+  name                       = var.name
+  target_resource_id         = azurerm_machine_learning_workspace.main.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  dynamic "log" {
+    for_each = [
+      for category in var.log_categories : {
+        name           = category.name
+        enabled        = category.enabled
+        retention_days = category.retention_days
+      }
+    ]
+    content {
+      category = log.value.name
+      enabled  = log.value.enabled
+
+      retention_policy {
+        enabled = log.value.enabled
+        days    = log.value.retention_days
+      }
+    }
+  }
+
+  dynamic "metric" {
+    for_each = [
+      for category in var.metric_categories : {
+        name           = category.name
+        enabled        = category.enabled
+        retention_days = category.retention_days
+      }
+    ]
+    content {
+      category = metric.value.name
+      enabled  = metric.value.enabled
+      retention_policy {
+        enabled = metric.value.enabled
+        days    = metric.value.retention_days
+      }
+    }
+  }
 }
