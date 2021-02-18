1aef29e2151020077a6436c64d3c371a0b9cee06
Mark Warneke <mark.warneke@microsoft.com>
2021-02-18 16:15:37 +0100

    Update main.tf

--- terraform/Microsoft/main.tf
+++ terraform/Microsoft/main.tf
@@ -38,7 +38,7 @@ module "load-more" {
 
   aks_default_nodepool_node_size  = "Standard_D2s_v3"
   aks_default_nodepool_node_count = 3
-  kubernetes_version              = "1.19.6"
+  kubernetes_version              = "1.18.0"
 }
 
 module "aml" {
